f46b331af7a2bf5ec5c341ef986508579885a998
tamakiii <tamakiii@users.noreply.github.com>
2020-07-01 22:59:09 +0900

    add WORKDIR and VOLUMES

--- docker.mk
+++ docker.mk
@@ -1,9 +1,11 @@
 .PHONY: help install dependencies build bash clean
 
 NAME := tamakiii-sandbox/hello-newrelic
+WORKDIR := /usr/local/app/hello-newrelic
 
 NEWRELIC_LICENSE_KEY ?= $(shell grep 'NEWRELIC_LICENSE_KEY' .env | awk -F= '{ print $$2 }')
 BUILD_ARGS ?= NEWRELIC_LICENSE_KEY=$(NEWRELIC_LICENSE_KEY)
+VOLUMES := $(CURDIR):$(WORKDIR)
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
@@ -23,7 +25,7 @@ build:
 	docker build -t $(NAME) $(foreach a,$(BUILD_ARGS),--build-arg $a) .
 
 bash:
-	docker run -it --rm $(NAME) $@
+	docker run -it --rm  -w $(WORKDIR) $(foreach v,$(VOLUMES),-v $v) $(NAME) $@
 
 clean:
 	docker image rm $(NAME)
