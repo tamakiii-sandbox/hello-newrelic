1bd84aa32995391e5970f6f03b75e0cf7af33112
tamakiii <tamakiii@users.noreply.github.com>
2020-07-01 22:55:14 +0900

    add license key

--- Dockerfile
+++ Dockerfile
@@ -18,3 +18,7 @@ RUN apt-get update && \
       && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
+
+ARG NEWRELIC_LICENSE_KEY
+COPY docker/newrelic.ini /usr/local/etc/php/conf.d/newrelic.ini
+RUN sed -ie "s/<your-key-goes-here>/${NEWRELIC_LICENSE_KEY}/" /usr/local/etc/php/conf.d/newrelic.ini
--- docker.mk
+++ docker.mk
@@ -2,18 +2,25 @@
 
 NAME := tamakiii-sandbox/hello-newrelic
 
+NEWRELIC_LICENSE_KEY ?= $(shell grep 'NEWRELIC_LICENSE_KEY' .env | awk -F= '{ print $$2 }')
+BUILD_ARGS ?= NEWRELIC_LICENSE_KEY=$(NEWRELIC_LICENSE_KEY)
+
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
 
 install: \
 	dependencies \
+	.env \
 	build
 
 dependencies:
 	type docker > /dev/null
 
+.env:
+	echo "NEWRELIC_LICENSE_KEY=$(NEWRELIC_LICENSE_KEY)" > $@
+
 build:
-	docker build -t $(NAME) .
+	docker build -t $(NAME) $(foreach a,$(BUILD_ARGS),--build-arg $a) .
 
 bash:
 	docker run -it --rm $(NAME) $@
